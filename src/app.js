46dd7310f9d6f78ed12368eda7d83416e1c9dda5
vv <89388361+vRyanv@users.noreply.github.com>
2022-10-19 00:17:55 +0700

    fisx bug view engine

--- src/app.js
+++ src/app.js
@@ -25,7 +25,7 @@ app.use(cookieParser())
 
 //Template engine
 app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'app/view'))
+app.set('views', path.join(__dirname, 'app/View'))
 
 //Rout init
 indexRoute(app)
